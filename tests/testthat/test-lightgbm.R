0b9e95ab3f2ff3bf4bf6ab9e4c646844251f0ace
simonpcouch <simonpatrickcouch@gmail.com>
2022-05-02 17:03:10 -0400

    add tests for adjusting main and engine arguments

--- tests/testthat/test-lightgbm.R
+++ tests/testthat/test-lightgbm.R
@@ -52,14 +52,14 @@ test_that("boost_tree with lightgbm",{
       categorical_feature = c(1L, 2L, 6L)
     )
 
-  params <- list(
+  params_1 <- list(
     objective = "regression"
   )
 
   lgbm_fit_1 <-
     lightgbm::lgb.train(
       data = peng_x,
-      params = params,
+      params = params_1,
       verbose = -1
     )
 
@@ -68,14 +68,70 @@ test_that("boost_tree with lightgbm",{
   expect_equal(pars_preds_1$.pred, lgbm_preds_1)
 
 
-  # do the same with primary argument
+  # regression, adjusting a primary argument
+  expect_error_free({
+    pars_fit_2 <-
+      boost_tree(trees = 20) %>%
+      set_engine("lightgbm") %>%
+      set_mode("regression") %>%
+      fit(bill_length_mm ~ ., data = penguins)
+  })
 
-  # do the same with engine argument
+  expect_error_free({
+    pars_preds_2 <-
+      predict(pars_fit_2, penguins)
+  })
+
+  params_2 <- list(
+    objective = "regression",
+    num_iterations = 20
+  )
+
+  lgbm_fit_2 <-
+    lightgbm::lgb.train(
+      data = peng_x,
+      params = params_2,
+      verbose = -1
+    )
+
+  lgbm_preds_2 <- predict(lgbm_fit_2, peng_m)
+
+  expect_equal(pars_preds_2$.pred, lgbm_preds_2)
+
+  # regression, adjusting an engine argument
+  expect_error_free({
+    pars_fit_3 <-
+      boost_tree() %>%
+      set_engine("lightgbm", lambda_l2 = .5) %>%
+      set_mode("regression") %>%
+      fit(bill_length_mm ~ ., data = penguins)
+  })
+
+  expect_error_free({
+    pars_preds_3 <-
+      predict(pars_fit_3, penguins)
+  })
+
+  params_3 <- list(
+    objective = "regression",
+    lambda_l2 = .5
+  )
+
+  lgbm_fit_3 <-
+    lightgbm::lgb.train(
+      data = peng_x,
+      params = params_3,
+      verbose = -1
+    )
+
+  lgbm_preds_3 <- predict(lgbm_fit_3, peng_m)
+
+  expect_equal(pars_preds_3$.pred, lgbm_preds_3)
 
   # ----------------------------------------------------------------------------
   # classification
 
-  # do the same with classification
+
 
 })
 
