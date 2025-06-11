9791475318aa676048b4e13015f58a7cbe830ddb
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-06-11 13:25:19 -0700

    add more objectives to process_objective_function()

--- R/lightgbm.R
+++ R/lightgbm.R
@@ -176,22 +176,38 @@ process_mtry <- function(
   feature_fraction_bynode
 }
 
+# https://lightgbm.readthedocs.io/en/latest/Parameters.html#core-parameters
 process_objective_function <- function(args, x, y) {
+  lvl <- levels(y)
+  lvls <- length(lvl)
   # set the "objective" param argument, clear it out from main args
   if (!any(names(args) %in% c("objective"))) {
     if (is.numeric(y)) {
       args$objective <- "regression"
     } else {
-      lvl <- levels(y)
-      lvls <- length(lvl)
       if (lvls == 2) {
-        args$num_class <- 1
         args$objective <- "binary"
       } else {
-        args$num_class <- lvls
         args$objective <- "multiclass"
       }
     }
+  }
+
+  if (args$objective == "binary" && is.null(args$num_class)) {
+    args$num_class <- 1
+  }
+
+  multiclass_obj <- c(
+    "multiclass",
+    "softmax",
+    "multiclassova",
+    "multiclass_ova",
+    "ova",
+    "ovr"
+  )
+
+  if (args$objective %in% multiclass_obj && is.null(args$num_class)) {
+    args$num_class <- lvls
   }
 
   args
