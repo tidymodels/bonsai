65ad39cc9ff0f3ccf12a433f5087168bb938945b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-06-11 13:57:41 -0700

    add test

--- tests/testthat/test-lightgbm.R
+++ tests/testthat/test-lightgbm.R
@@ -348,6 +348,30 @@ test_that("bonsai correctly determines objective when label is a factor", {
   expect_equal(bst$params$num_class, 3)
 })
 
+test_that("bonsai correctly determines num_classes when objective is set", {
+  skip_if_not_installed("lightgbm")
+  skip_if_not_installed("modeldata")
+
+  suppressPackageStartupMessages({
+    library(lightgbm)
+    library(dplyr)
+  })
+
+  data("penguins", package = "modeldata")
+  penguins <- penguins[complete.cases(penguins), ]
+
+  expect_no_error({
+    bst <- train_lightgbm(
+      x = penguins[, c("bill_length_mm", "bill_depth_mm")],
+      y = penguins[["species"]],
+      num_iterations = 5,
+      verbose = -1L,
+      objective = "multiclassova"
+    )
+  })
+  expect_identical(bst$params$objective, "multiclassova")
+  expect_identical(bst$params$num_class, 3L)
+})
 
 test_that("bonsai handles mtry vs mtry_prop gracefully", {
   skip_if_not_installed("modeldata")
